d3c1bac016e84eeee7e1c756dff6c1bbaa830810
Colin Simmons <colin.r.simmons@gmail.com>
2016-09-09 12:33:49 +0100

    remove uneeded tests and enpoints for walcies

--- hello.rb
+++ hello.rb
@@ -3,16 +3,3 @@ require 'sinatra'
 get '/' do
   "Hello, World!\n"
 end
-
-post '/crash' do
-  Process.kill 'KILL', Process.pid
-end
-
-post '/exit' do
-  Process.kill 'INT', Process.pid
-end
-
-post '/kill-haproxy' do
-  pid = exec("pidof haproxy")
-  Process.kill 'KILL', pid
-end
